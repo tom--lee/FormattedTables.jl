ae13a7273343a4b66f66ee4fcf9e69af99077b5f
Tom Lee <contact@tomlee.com.au>
2018-10-14 20:54:45 +1000

    Implement FormattedTables.jl

---
+++ src/FormattedTables.jl
@@ -0,0 +1,84 @@
+module FormattedTables
+
+using Format
+using Tables
+
+"""
+    write(
+        file::Union{IO,String},
+        table;
+        delim = ' ',
+        column_names=Tables.schema(table).names,
+        formatters=Dict(name=>"" for name in column_names),
+        header_fmt=Dict(name=>"" for name in column_names),
+    )
+Write a [Tables.jl interface input](https://github.com/JuliaData/Tables.jl)
+to a stream or a named file using the [format specifiers](https://github.com/JuliaString/Format.jl)
+provided via the `formatters` keyword argument. The `formatters` argument must
+be a dictionary, named tuple, or similar collection that can be indexed using
+the column names of the table.
+
+Keyword arguments include:
+* `delim`: A delimiter to print between list items.
+* `column_names`: An iterable collection containing, in order, the names (as
+  `Symbol`s) of the columns of the table to be written.
+* `formatters`: A dictionary, named tuple, or similar collection that maps 
+  the the names (`Symbol`s) of the columns to be written to a `Format.FormatSpec`
+  used to format the column.
+* `formatters`: A dictionary, named tuple, or similar collection that maps 
+  the the names (`Symbol`s) of the columns to be written to a `Format.FormatSpec`
+  used to format the column.
+* `header_fmt`: A dictionary, named tuple, or similar collection that maps 
+  the the names (`Symbol`s) of the columns to be written to a `Format.FormatSpec`
+  used to format the column headers. Set to `nothing` to skip writing column
+  headers.
+"""
+function write end
+
+function write(
+    filename::String,
+    table;
+    delim = ' ',
+    column_names=Tables.schema(table).names,
+    formatters=Dict(name=>"" for name in column_names),
+    header_fmt=Dict(name=>"" for name in column_names),
+)
+    open(filename, "w") do f
+        write(f, table, delim=delim, column_names=column_names, formatters=formatters, header_fmt=header_fmt)
+    end
+end
+
+function write(
+    io::IO,
+    table;
+    delim = " ",
+    column_names=Tables.schema(table).names,
+    formatters=Dict(name=>FormatSpec("") for name in column_names),
+    header_fmt=Dict(name=>FormatSpec("") for name in column_names),
+)
+    if header_fmt !== nothing
+        n = 0
+        for name in column_names
+            printfmt(io, header_fmt[name], string(name))
+            n+=1
+            if n<length(column_names)
+                print(io, delim)
+            end
+        end
+        print(io, '\n')
+    end
+    for row in Tables.rows(table)
+        n = 0
+        for name in column_names
+            printfmt(io, formatters[name], getproperty(row, name))
+            n+=1
+            if n<length(column_names)
+                print(io, delim)
+            end
+        end
+        print(io, '\n')
+    end
+    nothing
+end
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,89 @@
+
+using FormattedTables
+
+using DataFrames
+using Format
+using CSV
+using Test
+
+data = CSV.read("testdata.csv") |> DataFrame
+
+b = PipeBuffer()
+
+const fs = FormatSpec
+
+formatters = (
+name=fs("10s"),
+height=fs("7.2f"),
+age=fs("3d"),
+)
+header_fmt = (
+name=fs("10s"),
+height=fs(">7s"),
+age=fs(">3s"),
+)
+
+b = PipeBuffer()
+FormattedTables.write(b, data)
+result = String(take!(b))
+println(result)
+expected="""name height age
+Alice 1.6 21
+Bob 1.83 40
+Claire 1.75 31
+David 1.5 25
+Edith 1.68 30
+"""
+@test expected == result
+
+b = PipeBuffer()
+FormattedTables.write( b, data, delim = " ", formatters = formatters, header_fmt = header_fmt)
+result = String(take!(b))
+println(result)
+expected = """name        height age
+Alice         1.60  21
+Bob           1.83  40
+Claire        1.75  31
+David         1.50  25
+Edith         1.68  30
+"""
+@test expected == result
+
+b = PipeBuffer()
+FormattedTables.write( b, data, delim = " ", column_names=(:age, :name), formatters = formatters, header_fmt = header_fmt)
+result = String(take!(b))
+println(result)
+expected = """age name      
+ 21 Alice     
+ 40 Bob       
+ 31 Claire    
+ 25 David     
+ 30 Edith     
+"""
+@test expected == result
+
+
+b = PipeBuffer()
+FormattedTables.write( b, data, delim = " ", formatters = formatters, header_fmt = nothing)
+result = String(take!(b))
+println(result)
+expected = """Alice         1.60  21
+Bob           1.83  40
+Claire        1.75  31
+David         1.50  25
+Edith         1.68  30
+"""
+@test expected == result
+
+FormattedTables.write("test-out.dat", data, delim = " ", formatters = formatters, header_fmt = header_fmt)
+result = open(x->read(x,String), "test-out.dat",)
+println(result)
+expected = """name        height age
+Alice         1.60  21
+Bob           1.83  40
+Claire        1.75  31
+David         1.50  25
+Edith         1.68  30
+"""
+@test expected == result
+rm("test-out.dat")
